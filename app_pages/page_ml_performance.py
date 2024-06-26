bd8ece59299637b189bacc4b7383672545eae585
Kiree Bellamy <kireebellamy@gmail.com>
2024-06-26 14:49:28 +0000

    Modelling and evaluation notebook

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -9,11 +9,9 @@ def page_ml_performance_metrics():
     version = 'v1'
 
     st.write("### Train, Validation and Test Set: Labels Frequencies")
-
     labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
     st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
     st.write("---")
-
 
     st.write("### Model History")
     col1, col2 = st.beta_columns(2)
